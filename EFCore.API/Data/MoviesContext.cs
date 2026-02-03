0c4b6d0cf93d6d78fc4a194a269ca08e49c48368
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2026-02-03 10:48:11 +0200

    Starter Project

---
+++ EFCore.API/Data/MoviesContext.cs
@@ -0,0 +1,9 @@
+﻿using EFCore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.API.Data;
+
+public class MoviesContext : DbContext
+{
+    public DbSet<Movie> Movies { get; set; }
+}
